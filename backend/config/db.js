68d17e554a49e019eab4142d7d8f80b6b9e26532
Juan Manuel Gonzalez <jmgonzalez.parana@gmail.com>
2026-04-17 14:33:07 -0300

    Configuración: Soporte para base de datos en la nube

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,19 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-});
+const pool = new Pool(
+    process.env.DATABASE_URL
+        ? {
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false }
+        }
+        : {
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT,
+        }
+);
 
 module.exports = pool;
